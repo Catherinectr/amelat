23dba72869e4c3eed7877b1c391f26df674bb2de
Catherinectr <nidhi.catherine.ctr@gmail.com>
2024-01-12 15:15:17 -0700

    final

--- scripts/join.js
+++ scripts/join.js
@@ -4,6 +4,7 @@
 })();
 window.onload = function() {
 document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault()
         emailjs.sendForm('service_q9a619n', 'template_91uht1y', this)
 });
 };
