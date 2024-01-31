a986788ed0642b1afa5dd0b3ecd8f9bde051e699
NicholasBoss <NicholasBoss0914@gmail.com>
2024-01-31 16:10:17 -0700

    fixed multiple submission errors

--- scripts/join.js
+++ scripts/join.js
@@ -6,5 +6,10 @@ window.onload = function() {
 document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault()
         emailjs.sendForm('service_q9a619n', 'template_91uht1y', this)
+        // disable the button
+        
+        const reg_button = document.getElementsByClassName('button');
+        // hide the button 
+        reg_button[0].style.display = 'none';
 });
 };
